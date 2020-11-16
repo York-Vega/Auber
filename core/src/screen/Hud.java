2becce277ca4c71fea592003a8baf95d22c3529d
George Grasham <geoed2000@gmail.com>
2020-11-16 19:00:04 +0000

    Removed unused import

--- core/src/screen/Hud.java
+++ core/src/screen/Hud.java
@@ -6,7 +6,6 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.*;
 import com.badlogic.gdx.utils.viewport.FitViewport;
-import com.badlogic.gdx.utils.viewport.StretchViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import screen.actors.HealthBar;
 import screen.actors.Teleport_Menu;
