8eed0f485c0f1ad7316d36550c02d265d9505873
Lewis-McKenzie <53063230+Lewis-McKenzie@users.noreply.github.com>
2020-11-21 11:16:17 +0000

    Fixed Menu Display

--- core/src/screen/actors/PauseMenu.java
+++ core/src/screen/actors/PauseMenu.java
@@ -17,29 +17,33 @@ public class PauseMenu {
     private boolean paused;
     private boolean exiting;
     TextButton resumeButton = new TextButton("Resume", skin);
-    TextButton exitButton = new TextButton("Exit", skin);
+    TextButton exitButton = new TextButton("Exit", skin);    
+    TextButton settingsButton = new TextButton("Settings", skin);
     Label titleLabel = new Label("Paused", skin);
 
     public PauseMenu() {
         this.pauseWindow = new Window("Pause", this.skin);
         pauseWindow.setMovable(false);        
 
-        pauseWindow.setSize(Gdx.graphics.getWidth()/5, Gdx.graphics.getHeight() * 2/3);
-        pauseWindow.setPosition(Gdx.graphics.getWidth()/2 - pauseWindow.getWidth()/2,Gdx.graphics.getHeight()/2 - pauseWindow.getHeight()/2);
+        pauseWindow.setSize(200, 500);
+        pauseWindow.setPosition(1280 / 2 - pauseWindow.getWidth()/2,720 / 2 - pauseWindow.getHeight()/2);
         pauseWindow.setVisible(false);
         pauseWindow.setLayoutEnabled(false);        
 
         //Adds actors to the menu
         resumeButton.setName("Resume");
         exitButton.setName("Exit");
+        settingsButton.setName("Settings");
         titleLabel.setName("Title");
 
         pauseWindow.add(resumeButton);
         pauseWindow.add(exitButton);
+        pauseWindow.add(settingsButton);
         pauseWindow.add(titleLabel);
-
-        pauseWindow.findActor("Resume").setPosition(this.pauseWindow.getWidth()/2 - resumeButton.getWidth()/2, this.pauseWindow.getHeight() * 7/10 - resumeButton.getHeight()/2);
-        pauseWindow.findActor("Exit").setPosition(this.pauseWindow.getWidth()/2 - exitButton.getWidth()/2, this.pauseWindow.getHeight() * 5/10 - exitButton.getHeight()/2);
+        
+        pauseWindow.findActor("Resume").setPosition(this.pauseWindow.getWidth()/2 - resumeButton.getWidth()/2, this.pauseWindow.getHeight() * 7/10 - resumeButton.getHeight()/2);        
+        pauseWindow.findActor("Settings").setPosition(this.pauseWindow.getWidth()/2 - settingsButton.getWidth()/2, this.pauseWindow.getHeight() * 5/10 - settingsButton.getHeight()/2);
+        pauseWindow.findActor("Exit").setPosition(this.pauseWindow.getWidth()/2 - exitButton.getWidth()/2, this.pauseWindow.getHeight() * 3/10 - exitButton.getHeight()/2);
         pauseWindow.findActor("Title").setPosition(this.pauseWindow.getWidth()/2 - titleLabel.getWidth()/2, this.pauseWindow.getHeight() * 9/10 - titleLabel.getHeight()/2);
 
         pauseWindow.setColor(Color.BLACK);
@@ -67,6 +71,13 @@ public class PauseMenu {
                 exiting = true;
             }
         });
+
+        settingsButton.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                // TODO: open the settings menu here
+            }
+        });
     }
 
     /**
@@ -74,7 +85,6 @@ public class PauseMenu {
      */
     public void hide() { 
         pauseWindow.setVisible(false);
-        paused = false;
     }
 
     public Window pauseWindow() {
