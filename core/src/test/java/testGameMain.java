3d41cb794cb0e0a29fe10f07d2168068a7619f3f
JamesHooper0820 <jameshooper0820@gmail.com>
2020-12-22 22:20:51 +0000

    improved test formatting

---
+++ core/src/test/java/testGameMain.java
@@ -0,0 +1,12 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class testGameMain {
+  // This is where you would create tests for the GameMain class
+  @Test
+  public void create (){
+    
+  }
+
+}
