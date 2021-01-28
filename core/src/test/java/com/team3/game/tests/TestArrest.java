9fa0f229d5e4d15666ed22b599fabe08892264dd
J0EKW <73997025+J0EKW@users.noreply.github.com>
2021-01-28 10:31:26 +0000

    Test update

---
+++ core/src/test/java/com/team3/game/tests/TestArrest.java
@@ -0,0 +1,11 @@
+package com.team3.game.tests;
+
+import com.team3.game.GdxTestRunner;
+import org.junit.runner.RunWith;
+import org.junit.Test;
+
+@RunWith(GdxTestRunner.class)
+public class TestArrest {
+    
+    
+}
