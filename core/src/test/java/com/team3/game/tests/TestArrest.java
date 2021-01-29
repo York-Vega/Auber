8ccb0802ab5d7b7f4ec1262125dbb10237b3b65d
J0EKW <73997025+J0EKW@users.noreply.github.com>
2021-01-29 17:30:33 +0000

    removed unused libraries

--- core/src/test/java/com/team3/game/tests/TestArrest.java
+++ core/src/test/java/com/team3/game/tests/TestArrest.java
@@ -2,15 +2,12 @@ package com.team3.game.tests;
 
 import static org.junit.Assert.assertTrue;
 
-import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
-import com.team3.game.map.Map;
 import com.team3.game.GdxTestRunner;
 import com.team3.game.characters.Player;
 import com.team3.game.characters.ai.Enemy;
-import com.team3.game.tools.ObjectContactListener;
+import com.team3.game.map.Map;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
