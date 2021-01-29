e8824f8fa3abc306815787edf70dbe9f0f5f1758
J0EKW <73997025+J0EKW@users.noreply.github.com>
2021-01-29 02:34:45 +0000

    further checkstyle changes 3

--- core/src/test/java/com/team3/game/tests/TestArrest.java
+++ core/src/test/java/com/team3/game/tests/TestArrest.java
@@ -2,8 +2,8 @@ package com.team3.game.tests;
 
 import static org.junit.Assert.assertTrue;
 
-import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.World;
 import com.team3.game.GdxTestRunner;
 import com.team3.game.characters.Player;
 import com.team3.game.characters.ai.Enemy;
