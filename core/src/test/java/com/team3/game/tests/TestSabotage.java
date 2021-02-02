de65c66e8f6470881f12513e09cf56dbee1ba1b7
J0EKW <73997025+J0EKW@users.noreply.github.com>
2021-02-02 01:31:32 +0000

    added some documentation

--- core/src/test/java/com/team3/game/tests/TestSabotage.java
+++ core/src/test/java/com/team3/game/tests/TestSabotage.java
@@ -21,6 +21,8 @@ public class TestSabotage {
   
   @Test
   public void testSabotage() {
+
+    //Instantiating necessary values to test sabotage
     CharacterRenderer.loadTextures();
     TmxMapLoader maploader = new TmxMapLoader();
     TiledMap map = maploader.load("Map/Map.tmx");
@@ -28,8 +30,11 @@ public class TestSabotage {
     World world = new World(new Vector2(0, 0), true);
     Enemy enemy = new Enemy(world, 0, 0);
     StationSystem system = new StationSystem(world, map, new Rectangle(0, 0, 0, 0), "healingPod");
+
+    //action of sabotage, should result in damage being dealt once
     enemy.sabotage(system);
 
+    //test to see if damage matches the expected value, and if damage was dealt at all
     assertEquals("Unexpected value in system health", (double)system.hp, 99.5, 0);
     assertFalse("No damage was dealt", system.hp == 100);
 
