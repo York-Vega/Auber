2677c5a6bff2983aa02849f2c00efd23e8a22e22
connorjdavies <74500603+connorjdavies@users.noreply.github.com>
2021-02-02 17:21:44 +0000

    Updated to match checkstyle

--- core/src/test/java/com/team3/game/tests/TestGenerateEnemy.java
+++ core/src/test/java/com/team3/game/tests/TestGenerateEnemy.java
@@ -1,20 +1,19 @@
 package com.team3.game.tests;
 
-import com.team3.game.GdxTestRunner;
-import com.team3.game.characters.ai.EnemyManager;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-
 import static org.junit.Assert.assertEquals;
 
-import com.team3.game.tools.CharacterRenderer;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.World;
+import com.team3.game.GdxTestRunner;
+import com.team3.game.characters.ai.EnemyManager;
 import com.team3.game.map.Map;
-import java.util.ArrayList;
 import com.team3.game.sprites.StationSystem;
+import com.team3.game.tools.CharacterRenderer;
+import java.util.ArrayList;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 
 
 @RunWith(GdxTestRunner.class)
