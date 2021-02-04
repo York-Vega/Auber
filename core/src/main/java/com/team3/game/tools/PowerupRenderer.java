2b51b7a47efcf1044cf7da8ee0ec5839a1df04d0
Daniel <dob9601@gmail.com>
2021-02-04 11:20:29 +0000

    More formatting

--- core/src/main/java/com/team3/game/tools/PowerupRenderer.java
+++ core/src/main/java/com/team3/game/tools/PowerupRenderer.java
@@ -87,123 +87,122 @@ import com.badlogic.gdx.utils.Array;
  */
 public class PowerupRenderer {
 
-    private static TextureAtlas atlas;
-    private static Object[][] animations;
+  private static TextureAtlas atlas;
+  private static Object[][] animations;
 
-    private Sprite sprite;
-    private State state;
-    private Boolean hidden;
-    private Float timeInState;
-    private TextureRegion currentFrame;
+  private Sprite sprite;
+  private State state;
+  private Boolean hidden;
+  private Float timeInState;
+  private TextureRegion currentFrame;
 
-    /**
-     * Sprites the CharacterRenderer can draw.
-     */
-    public enum Sprite {
-        POWERUP(0);
+  /**
+   * Sprites the CharacterRenderer can draw.
+   */
+  public enum Sprite {
+    POWERUP(0);
 
-        int index;
+    int index;
 
-        Sprite(int i) {
-            this.index = i;
-        }
+    Sprite(int i) {
+      this.index = i;
+    }
+  }
+
+  /**
+   * Possible character animation cycles.
+   */
+  public enum State {
+    speed(0), vision(1), repair(2), heal(3), arrest(4), hidden(5);
+
+    int index;
+
+    State(int i) {
+      this.index = i;
+    }
+  }
+
+  /**
+   * Loads all textures needed by the Character Render should be called before game starts.
+   */
+  public static void loadTextures() {
+    atlas = new TextureAtlas("sprites/powerupSprites.atlas");
+
+    animations = new Object[Sprite.values().length][State.values().length];
+    for (Sprite s : Sprite.values()) {
+      System.out.println(s);
+      for (State t : State.values()) {
+        System.out.println(t);
+        Array<AtlasRegion> tex = atlas.findRegions(s.name() + "_" + t.name());
+        animations[s.index][t.index] =
+          new Animation<TextureRegion>(0.10f, tex, PlayMode.LOOP);
+      }
+    }
+  }
+
+  /**
+   * Creates a new Character renderer instance.
+
+   * @param sprite The CharacterRenderer.Sprite to use when drawing the character
+   */
+  public PowerupRenderer(Sprite sprite) {
+    this.sprite = sprite;
+    this.hidden = false;
+    state = State.speed;
+    timeInState = 0f;
+  }
+
+  private PowerupRenderer.State nextState;
+
+  /**
+   * Updates the animation based on time passed and character movement.
+
+   * @param dt Time in seconds since last update call
+   * @param type Type of powerup.
+   */
+  @SuppressWarnings("unchecked")
+  public void update(float dt, String type) {
+    timeInState += dt;
+
+    int temp = 0;
+    switch(type) {
+      case "speed":
+        temp = 0;
+        break;
+      case "vision":
+        temp = 1;
+        break;
+      case "repair":
+        temp = 2;
+        break;
+      case "heal":
+        temp = 3;
+        break;
+      case "arrest":
+        temp = 4;
+        break;
+      case "hidden":
+        temp = 5;
+      default:
+        break;
     }
 
-    /**
-     * Possible character animation cycles.
-     */
-    public enum State {
-        speed(0), vision(1), repair(2), heal(3), arrest(4), hidden(5);
+    Animation<TextureRegion> animation = (Animation<TextureRegion>) animations[sprite.index][temp];
+    currentFrame = animation.getKeyFrame(timeInState, true);
+  }
 
-        int index;
+  /**
+   * Renders the character at the given position using the given SpriteBatch.
 
-        State(int i) {
-            this.index = i;
-        }
+   * @param position The bottom left of the character
+   * @param batch SpriteBatch to be drawn with, make sure to use SpriteBatch.begin() before hand
+   */
+  public void render(Vector2 position, SpriteBatch batch) {
+    int width = currentFrame.getRegionWidth() * 2;
+    int height = currentFrame.getRegionHeight() * 2;
+    if (!this.hidden) {
+      batch.draw(currentFrame, position.x, position.y, width, height);
     }
-
-    /**
-     * Loads all textures needed by the Character Render should be called before game starts.
-     */
-    public static void loadTextures() {
-        atlas = new TextureAtlas("sprites/powerupSprites.atlas");
-
-        animations = new Object[Sprite.values().length][State.values().length];
-        for (Sprite s : Sprite.values()) {
-            System.out.println(s);
-            for (State t : State.values()) {
-                System.out.println(t);
-                Array<AtlasRegion> tex = atlas.findRegions(s.name() + "_" + t.name());
-                animations[s.index][t.index] =
-                        new Animation<TextureRegion>(0.10f, tex, PlayMode.LOOP);
-            }
-        }
-    }
-
-    /**
-     * Creates a new Character renderer instance.
-
-     * @param sprite The CharacterRenderer.Sprite to use when drawing the character
-     */
-    public PowerupRenderer(Sprite sprite) {
-        this.sprite = sprite;
-        this.hidden = false;
-        state = State.speed;
-        timeInState = 0f;
-    }
-
-    private PowerupRenderer.State nextState;
-
-    /**
-     * Updates the animation based on time passed and character movement.
-
-     * @param dt Time in seconds since last update call
-     * @param type Type of powerup.
-     */
-    @SuppressWarnings("unchecked")
-    public void update(float dt, String type) {
-        timeInState += dt;
-
-        int temp = 0;
-        switch(type) {
-            case "speed":
-                temp = 0;
-                break;
-            case "vision":
-                temp = 1;
-                break;
-            case "repair":
-                temp = 2;
-                break;
-            case "heal":
-                temp = 3;
-                break;
-            case "arrest":
-                temp = 4;
-                break;
-            case "hidden":
-                temp = 5;
-            default:
-                break;
-        }
-
-        Animation<TextureRegion> animation =
-                (Animation<TextureRegion>) animations[sprite.index][temp];
-        currentFrame = animation.getKeyFrame(timeInState, true);
-    }
-
-    /**
-     * Renders the character at the given position using the given SpriteBatch.
-
-     * @param position The bottom left of the character
-     * @param batch SpriteBatch to be drawn with, make sure to use SpriteBatch.begin() before hand
-     */
-    public void render(Vector2 position, SpriteBatch batch) {
-        int width = currentFrame.getRegionWidth() * 2;
-        int height = currentFrame.getRegionHeight() * 2;
-        if (!this.hidden) {
-            batch.draw(currentFrame, position.x, position.y, width, height);
-        }
-    }
+  }
 
 }
