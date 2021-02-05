e05da901267de58759e505c7f15aba2ad8a0056c
connorjdavies <74500603+connorjdavies@users.noreply.github.com>
2021-02-05 12:42:23 +0000

    Add TestAbilities

---
+++ core/src/test/java/com/team3/game/tests/TestAbilities.java
@@ -0,0 +1,24 @@
+package com.team3.game.tests;
+
+import static org.junit.Assert.assertEquals;
+
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.World;
+import com.team3.game.GdxTestRunner;
+import com.team3.game.characters.Player;
+import com.team3.game.characters.ai.Enemy;
+import com.team3.game.map.Map;
+import com.team3.game.tools.CharacterRenderer;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(GdxTestRunner.class)
+public class TestAbilities { 
+  
+  @Test
+  public void testSlowDownPlayer() throws Exception {
+      
+  }
+}
