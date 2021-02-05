b4a41c4e00eb515bcc45ed9b6073c8ef26d20ec0
connorjdavies <74500603+connorjdavies@users.noreply.github.com>
2021-02-05 12:53:52 +0000

    Add test for Slow ability

--- core/src/test/java/com/team3/game/tests/TestAbilities.java
+++ core/src/test/java/com/team3/game/tests/TestAbilities.java
@@ -19,6 +19,16 @@ public class TestAbilities {
   
   @Test
   public void testSlowDownPlayer() throws Exception {
-      
+    CharacterRenderer.loadTextures();
+    TmxMapLoader maploader = new TmxMapLoader();
+    TiledMap map = maploader.load("Map/Map.tmx");
+    Map.create(map);
+    World world = new World(new Vector2(0, 0), true);
+    Player player = new Player(world, 0, 0);
+    Enemy enemy = new Enemy(world, 0, 0);
+    Float speed = player.speed;
+    enemy.ability.slowDownPlayer(player);
+
+    assertEquals("Error, player not slowed",(speed*0.5f), player.speed, 0.0f);
   }
 }
